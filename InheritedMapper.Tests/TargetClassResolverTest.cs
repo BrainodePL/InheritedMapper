23f4ba19c8e5feb3e7e610142bcbe74c16c152a0
Jakub Zohar Rutkowski <kuba@skb.pl>
2023-02-03 06:49:47 +0100

    Initial commit - Inherited mapper with unit tests

---
+++ InheritedMapper.Tests/TargetClassResolverTest.cs
@@ -0,0 +1,153 @@
+﻿using System.Reflection;
+using Nelibur.ObjectMapper;
+using NUnit.Framework;
+
+namespace InheritedMapper.Tests
+{
+    public class TargetClassResolverTest
+    {
+        [Test]
+        public void TryCreate_ShouldReturnObject_WithPropertyValuesAsInSampleViewModelProperties()
+        {
+            var sampleViewModel = new SampleViewModel()
+            {
+                Guid = Guid.NewGuid(),
+                SampleViewModelType = "DerivedSample",
+                CreatedDate = new DateTime(2023, 01, 23, 23, 23, 23),
+                ElementPrice = 1232.23m
+            };
+
+            var derivedSample = VmToInheritedModel.TryCreate<SampleViewModel, SampleBase>
+                (sampleViewModel, sampleViewModel.SampleViewModelType, typeof(SampleBase));
+
+            Assert.True(sampleViewModel.Guid == derivedSample.Guid);
+            Assert.True(sampleViewModel.CreatedDate == new DateTime(2023, 01, 23, 23, 23, 23));
+            Assert.True(sampleViewModel.ElementPrice == derivedSample.ElementPrice);
+        }
+
+        [Test]
+        public void TryCreate_ShouldReturnObject_WithPropertyValuesAsInSampleViewModelProperties_2()
+        {
+            var sampleViewModel = new SampleViewModel()
+            {
+                Guid = Guid.NewGuid(),
+                SampleViewModelType = "DerivedSecondSample",
+                CreatedDate = new DateTime(2023, 01, 23, 23, 23, 23),
+                ElementPrice = 1232.23m,
+                OnlySecondDerivedSampleValue = "test value"
+            };
+
+            DerivedSecondSample derivedSecondSample = (DerivedSecondSample)VmToInheritedModel.TryCreate<SampleViewModel, SampleBase>
+                (sampleViewModel, sampleViewModel.SampleViewModelType, typeof(SampleBase));
+
+            Assert.True(sampleViewModel.Guid == derivedSecondSample.Guid);
+            Assert.True(sampleViewModel.CreatedDate == new DateTime(2023, 01, 23, 23, 23, 23));
+            Assert.True(sampleViewModel.ElementPrice == derivedSecondSample.ElementPrice);
+            Assert.True(sampleViewModel.OnlySecondDerivedSampleValue == derivedSecondSample.OnlySecondDerivedSampleValue);
+        }
+
+        [Test]
+        public void TryCreate_ShouldThrowDidNotFoundInheritedBaseClassException_IfWeAreNotAbleToFindAnyDerivedClassWhoHasNameLikeSpecifiedInViewModelProperty()
+        {
+            var targetDerivedClassTypeName = "DerivedSampleBlaBla";
+            var sampleViewModel = new SampleViewModel() { Guid = Guid.NewGuid(), SampleViewModelType = targetDerivedClassTypeName };
+
+            Assert.Throws<VmToInheritedModel.DidNotFoundInheritedBaseClassException>(() => VmToInheritedModel.TryCreate<SampleViewModel, SampleBase>
+                (sampleViewModel, sampleViewModel.SampleViewModelType, typeof(SampleBase)));
+        }
+
+        [Test]
+        public void TryCreate_ShouldCreateInctanceOfTypeSpecifiedInViewModelStringProperty_EvenIfBaseTypeHasAdditionalPropertiesOfTypeClasses()
+        {
+            var targetDerivedClassTypeName = "ExpandedBase";
+            var sampleViewModel = new SampleViewModel() { Guid = Guid.NewGuid(), SampleViewModelType = targetDerivedClassTypeName };
+
+            var result = VmToInheritedModel.TryCreate<SampleViewModel, ExpandedBaseAbstract>
+                (sampleViewModel, sampleViewModel.SampleViewModelType, typeof(ExpandedBaseAbstract));
+
+            Assert.IsInstanceOf<ExpandedBase>(result);
+        }
+
+        [Test]
+        public void TryCreate_ShouldCreateInctanceOfTypeSpecifiedInViewModelStringProperty_WithValuesConfiguredByTinyMapper()
+        {
+            var targetDerivedClassTypeName = "ExpandedBase";
+            var sampleViewModel = new SampleViewModel() { Guid = Guid.NewGuid(), SampleViewModelType = targetDerivedClassTypeName };
+            TinyMapper.Bind<SampleViewModel, ExpandedBase>(config =>
+            {
+                config.Bind(x => x.SampleViewModelType, x => x.ProductName);
+            });
+
+            var result = VmToInheritedModel.TryCreate<SampleViewModel, ExpandedBaseAbstract>
+                (sampleViewModel, sampleViewModel.SampleViewModelType, typeof(ExpandedBaseAbstract));
+
+            Assert.True(result.ProductName == targetDerivedClassTypeName);
+            Assert.True(result.SampleBase == null);
+        }
+
+        [Test]
+        public void TryCreate_ShouldThrowDidNotFoundBaseAbstractClassException_WhenTargetBaseClassIsNotAbstractClass()
+        {
+            var targetDerivedClassTypeName = "DerivedSample";
+            var sampleViewModel = new SampleViewModel() { Guid = Guid.NewGuid(), SampleViewModelType = targetDerivedClassTypeName };
+
+            Assert.Throws<VmToInheritedModel.DidNotFoundBaseAbstractClassException>(() => VmToInheritedModel.TryCreate<SampleViewModel, BaseNotAbstractClass>
+                (sampleViewModel, sampleViewModel.SampleViewModelType, typeof(BaseNotAbstractClass)));
+        }
+
+        [Test]
+        public void TryCreate_ShouldThrowException_WhenThereIsNoClassInSolutionWhichHasNameLikeSpecidedInViewModelProperty()
+        {
+            var targetDerivedClassTypeName = "DerivedSample";
+            var sampleViewModel = new SampleViewModel() { Guid = Guid.NewGuid(), SampleViewModelType = targetDerivedClassTypeName };
+
+            var derivedSample = VmToInheritedModel.TryCreate<SampleViewModel, SampleBase>
+                (sampleViewModel, sampleViewModel.SampleViewModelType, typeof(SampleBase));
+
+            Assert.IsInstanceOf<DerivedSample>(derivedSample);
+        }
+
+
+    }
+
+    public abstract class ExpandedBaseAbstract
+    {
+        public Guid Guid { get; set; }
+        public string ProductName { get; set; }
+        public SampleBase SampleBase { get; set; }
+    }
+    
+    public class ExpandedBase : ExpandedBaseAbstract
+    {
+    }
+    
+    public class BaseNotAbstractClass
+    {
+    }
+
+    public class SampleViewModel
+    {
+        public Guid Guid { get; set; }
+        public string SampleViewModelType { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public decimal ElementPrice { get; set; }
+        public string OnlySecondDerivedSampleValue { get; set; }
+    }
+
+    public abstract class SampleBase
+    {
+        public Guid Guid { get; set; }
+        public decimal ElementPrice { get; set; }
+    }
+
+    public class DerivedSample : SampleBase
+    {
+
+    }
+    public class DerivedSecondSample : SampleBase
+    {
+        public string OnlySecondDerivedSampleValue { get; set; }
+    }
+
+
+}
---
+++ InheritedMapper/BaseClassHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+
+namespace InheritedMapper
+{
+    public class BaseClassHelper
+    {
+        public static Type[] GetAllDerivedClassesFromBaseClass(Type baseClassType)
+        {
+            Assembly asm = baseClassType.GetTypeInfo().Assembly;
+
+            var types = asm.DefinedTypes.Where(x => x.BaseType == (baseClassType));
+            var result = types.Select(x => x.AsType());
+            return result.ToArray();
+        }
+
+        public static Type[] GetAllDerivedGenericClassesFromBaseClass(Type baseClassType)
+        {
+            Assembly asm = baseClassType.GetTypeInfo().Assembly;
+            var types = asm.DefinedTypes.Where(x => x.IsClass && x.BaseType.Name == (baseClassType.Name));
+            var result = types.Select(x => x.AsType());
+
+            return result.ToArray();
+        }
+
+        public static Type GetDerivedType(Type baseClassType, string derivedClassName)
+        {
+            Type[] targetClass = GetAllDerivedClassesFromBaseClass(baseClassType);
+            var realizationClassType = targetClass.First(type => type.Name == derivedClassName);
+            return realizationClassType;
+        }
+    }
+}
