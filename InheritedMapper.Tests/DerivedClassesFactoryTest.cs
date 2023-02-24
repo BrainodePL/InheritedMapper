9899d442f09e91185672110824fa7b6fa1d88e87
Jakub Zohar Rutkowski <kuba@skb.pl>
2023-02-24 09:21:16 +0100

    Simple factory for creating derived classes from base class

---
+++ InheritedMapper.Tests/DerivedClassesFactoryTest.cs
@@ -0,0 +1,17 @@
+﻿using InheritedMapper.Tests.SampleClasses;
+using NUnit.Framework;
+
+namespace InheritedMapper.Tests;
+
+public class DerivedClassesFactoryTest
+{
+    [Test]
+    public void DerivedClassesFactoryCreate_shouldCreateAllDerivedClassesFromSpecifiedBaseClass()
+    {
+        var allDerivedClassesCount = BaseClassHelper.GetAllDerivedClassesFromBaseClass(typeof(SampleBase));
+        
+        var derivedClasses = DerivedClassesFactory.Create<SampleBase>(typeof(SampleBase));
+        
+        Assert.True(derivedClasses.Count() == allDerivedClassesCount.Length);
+    }
+}
---
+++ InheritedMapper/DerivedClassesFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritedMapper
+{
+    public class DerivedClassesFactory
+    {
+        public static IEnumerable<T> Create<T>(Type type) where T : class
+        {
+            foreach (var derivedType in BaseClassHelper.GetAllDerivedClassesFromBaseClass(type))
+            {
+                yield return (T) Activator.CreateInstance(derivedType);
+            }
+        }
+    }
+}
