75a8eefae8b7ff6d66f7f2f75953ad6b6e35d7da
Jakub Zohar Rutkowski <kuba@skb.pl>
2023-03-08 22:32:25 +0100

    Correct vm mappings

--- InheritedMapper/VmToDerivedVm.cs
+++ InheritedMapper/VmToDerivedVm.cs
@@ -12,17 +12,13 @@ namespace InheritedMapper
             PropertyInfo[] properties = baseVmObject.GetType().GetProperties();
 
             var className = baseVmObject.GetType().Name;
-            if (className.EndsWith("Vm") || className.EndsWith("Im") || className.EndsWith("Um") || className.EndsWith("Dm"))
-            {
-                className = className.Substring(0, className.Length - 2);
-            }
 
+            var name = className + "Type";
             foreach (var property in properties)
             {
-                var name = className + "Type";
                 if (property.Name == name)
                 {
-                    var targetClassName = property.GetValue(baseVmObject)?.ToString();
+                    var targetClassName = property.GetValue(baseVmObject)?.ToString() + "Vm";
                     var derivedType = BaseClassHelper.GetDerivedType(typeof(T), targetClassName);
                     var resultObject = TinyMapper.Map(baseVmObject.GetType(), derivedType, baseVmObject);
 
