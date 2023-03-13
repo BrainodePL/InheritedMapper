e074ec29b1b91a2fb93700f71b2310bb18354ff9
Jakub Zohar Rutkowski <kuba@skb.pl>
2023-03-13 15:25:13 +0100

    Derived as T overload

---
+++ InheritedMapper/DerivedAsTFromStringClassResolver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace InheritedMapper
+{
+    public class DerivedAsTFromStringClassResolver<T> where T : class
+    {
+        public T GetInstance(string className)
+        {
+            var targetType = DerivedClassesFactory.CreateDerivedClasses<T>().First(x => String.Equals(x.GetType().Name, className, StringComparison.OrdinalIgnoreCase)).GetType();
+            return (T) Activator.CreateInstance(targetType);
+        }
+    }
+}
